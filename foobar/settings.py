264d07d4780e3f66d54d2ab645eb72f8f5469ddb
makeabug <makeabug@gmail.com>
2014-09-23 23:14:18 +0800

    locale

--- foobar/settings.py
+++ foobar/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -88,3 +89,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/foobar/static/'
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale/'))
--- readers/models.py
+++ readers/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True, verbose_name=_('Category Name'))
